cc18406b2fcf092c027cc632e5427fd342ac83e0
Mart de Graaf <m.degraaf@4dotnet.nl>
2022-11-18 23:42:47 +0100

    fix test and test for broken mermaid diagram

--- tests/posts/blogging-agile.spec.ts
+++ tests/posts/blogging-agile.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('posts agile blogging', async ({ page }) => {
-  await page.goto('/posts/blogging-agile');
+  await page.goto('posts/howtoget');
 
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Blogging agile/);
 
   await page.screenshot({ path: 'blogging-agile.png', fullPage: true });
+
+  //check that mermaid diagram loads
+  //Syntax error in graph mermaid version 9.2.2
+  await expect(page.locator('.mermaid')).not.toHaveText('Syntax error in graph');
 });
