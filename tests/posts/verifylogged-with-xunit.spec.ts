31144a8d5e9178f42678330894c4bf1650b4f0e9
Mart de Graaf <m.degraaf@4dotnet.nl>
2022-11-18 22:47:27 +0100

    fix test after grammar fix

--- tests/posts/verifylogged-with-xunit.spec.ts
+++ tests/posts/verifylogged-with-xunit.spec.ts
@@ -5,7 +5,7 @@ test('posts verifylogged-with-xunit', async ({ page }) => {
 
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/How to verify that ILogger actually logged an error?/);
+  await expect(page).toHaveTitle(/How to verify that ILogger logged an error?/);
 
   await page.screenshot({ path: 'verifylogged-with-xunit.png', fullPage: true });
 });
