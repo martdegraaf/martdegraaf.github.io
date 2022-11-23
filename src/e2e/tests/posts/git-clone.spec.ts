e74eb6486e853618ee0ea75b83ecaece8f15863c
Mart de Graaf <m.degraaf@4dotnet.nl>
2022-11-23 22:42:12 +0100

    move all tests to src/e2e
    Folder could have nicer name but if this works then i need to only change folder and 1 ref

---
+++ src/e2e/tests/posts/git-clone.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Git clone blog page', () => {
+    test('posts clone git repos', async ({ page }) => {
+        await page.goto('posts/git-clone-all-repos-azure-devops');
+
+
+        // Expect a title "to contain" a substring.
+        await expect(page).toHaveTitle(/Clone All Git Repos from Azure Devops/);
+
+        await page.screenshot({ path: 'clone.png', fullPage: true });
+    });
+});
