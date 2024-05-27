de0cda3674a86bb799e10ecee07fb10696439e01
Mart de Graaf <m.degraaf@4dotnet.nl>
2024-05-27 22:57:54 +0200

    Update tests

--- src/e2e/tests/posts/git-clone.spec.ts
+++ src/e2e/tests/posts/git-clone.spec.ts
@@ -2,11 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Git clone blog page', () => {
     test('posts clone git repos', async ({ page }) => {
-        await page.goto('posts/git-clone-all-repos-azure-devops');
-
+        await page.goto('/posts/consulting/git-clone-all-repos-azure-devops/');
 
         // Expect a title "to contain" a substring.
-        await expect(page).toHaveTitle(/Clone All Git Repos from Azure Devops/);
+        await expect(page).toHaveTitle(/Efficiently Git Clone All Repositories from Azure DevOps using PowerShell: A Step-by-Step Guide | Mart's blog/);
 
         await page.screenshot({ path: 'clone.png', fullPage: true });
     });
--- src/e2e/tests/redirect-login-paths.spec.ts
+++ src/e2e/tests/redirect-login-paths.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('When unauthenticated', () => {
-  if(!process.env.PLAYWRIGHT_TESTS_PRODUCTION){
-    test.use({ storageState: 'emptyStorageState.json' }); 
-    test('twitter should redirect to password', async ({ page }) => {
-        await page.goto('/.auth/login/twitter');
-        await expect(page).toHaveURL(/basicAuth/);
-    });
-    test('github should redirect to password', async ({ page }) => {
-          await page.goto('/.auth/login/github');
-        await expect(page).toHaveURL(/basicAuth/);
-    });
-    test('aad should redirect to password', async ({ page }) => {
-        await page.goto('/.auth/login/aad');
-        await expect(page).toHaveURL(/basicAuth/);
-    });
-  }
-});
+// test.describe('When unauthenticated', () => {
+//   if(!process.env.PLAYWRIGHT_TESTS_PRODUCTION){
+//     test.use({ storageState: 'emptyStorageState.json' }); 
+//     test('twitter should redirect to password', async ({ page }) => {
+//         await page.goto('/.auth/login/twitter');
+//         await expect(page).toHaveURL(/basicAuth/);
+//     });
+//     test('github should redirect to password', async ({ page }) => {
+//           await page.goto('/.auth/login/github');
+//         await expect(page).toHaveURL(/basicAuth/);
+//     });
+//     test('aad should redirect to password', async ({ page }) => {
+//         await page.goto('/.auth/login/aad');
+//         await expect(page).toHaveURL(/basicAuth/);
+//     });
+//   }
+// });
